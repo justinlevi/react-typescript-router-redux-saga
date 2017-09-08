042c5e1b6cc373e80a2a86baa2b61f31f75946b5
Justin Winter <justinlevi@gmail.com>
2017-09-08 10:00:08 -0400

    re-writing reducer based on tutorial https://www.youtube.com/watch?v=OO3uj1JdP4s&t=2050s

--- src/components/Hello/reducer.tsx
+++ src/components/Hello/reducer.tsx
@@ -9,6 +9,9 @@
 // be to ensure that increments raise the enthusiasm level by 1, and 
 // that decrements reduce the enthusiasm level by 1, but that the level 
 // never falls below 1.
+
+import { Reducer } from 'redux';
+
 import { Action } from './Actions';
 import ActionTypes from './ActionTypes';
 
@@ -19,8 +22,29 @@ export const initialState: HelloState = {
   name: 'Typescript',
 };
 
+// Just renaming the StateProps from the Hello Component
 export type HelloState = HelloState;
 
+export const reducer: Reducer<HelloState> = (state = initialState, action: Action) => {
+  const val = state.enthusiasmLevel === undefined ? 0 : state.enthusiasmLevel as number;
+
+  switch (action.type) {
+    case ActionTypes.INCREMENT_ENTHUSIASM:
+      return { ...state, enthusiasmLevel: val + 1 };
+    case ActionTypes.DECREMENT_ENTHUSIASM:
+      return { ...state, enthusiasmLevel: Math.max(1, val - 1) };
+    case ActionTypes.INCREMENT_BY_ENTHUSIASM:
+      return { ...state, enthusiasmLevel: val + action.by };
+    case ActionTypes.DECREMENT_BY_ENTHUSIASM:
+      return { ...state, enthusiasmLevel: Math.max(1, val - action.by) };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
+
+/*
 export default function reducer(state: HelloState = initialState, action: Action): HelloState {
   const val = state.enthusiasmLevel === undefined ? 0 : state.enthusiasmLevel as number;
 
@@ -37,6 +61,7 @@ export default function reducer(state: HelloState = initialState, action: Action
   }
   return state;
 }
+*/
 
 // Notice that we're using the object spread (...state) which allows us to 
 // create a shallow copy of our state, while replacing the enthusiasmLevel. 
